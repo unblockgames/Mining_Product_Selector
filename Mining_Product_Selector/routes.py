57af1e3d911c58f9dd79996bb6bb18a4e30ffe88
Jason Casey <unblockgames@gmail.com>
2021-11-08 14:57:43 -0600

    Refactored code to be more readable

--- Mining_Product_Selector/routes.py
+++ Mining_Product_Selector/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, session, jsonify
+from flask import render_template, request
 from Mining_Product_Selector import app
 import mysql.connector
 import json
@@ -13,16 +13,15 @@ def home():
 @app.route('/log', methods=['POST'])
 def log():
     if request.method == 'POST':
-        data = json.loads(request.form['data'])
-        jsonDump = json.dumps(data, indent=4)
+        data = json.dumps(json.loads(request.form['data']), indent=4)
         with open('Mining_Product_Selector/config.json', 'r') as f:
             config = json.load(f)
         cnx = mysql.connector.connect(user=config['Database']['Username'], password=config['Database']['Password'],
                                       host=config['Database']['Host'], database=config['Database']['Database'])
         cursor = cnx.cursor()
-        insertStatement = "INSERT INTO events (jsonlog) VALUES ({})"
-        testvar = '\'' + jsonDump + '\''
-        insertStatement = insertStatement.format(testvar)
-        cursor.execute(insertStatement)
+        sql = "INSERT INTO events (jsonlog) VALUES ({})"
+        data = '\'' + data + '\''
+        sql = sql.format(data)
+        cursor.execute(sql)
         cnx.commit()
     return "Success"
